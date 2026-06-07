1204950555357c25b22f6ce01e7d1e40c6feee94
Brandon Birkholz <brandonjbirkholz@gmail.com>
2026-06-06 22:24:21 -0500

    feat(build): Add support for building the Windows version on Linux

--- tools/build/sign-windows.js
+++ tools/build/sign-windows.js
@@ -1,6 +1,6 @@
-// Source: https://github.com/ShabadOS/desktop/pull/266/files
-
 const { execSync } = require('child_process');
+const { platform } = require('os');
+const { renameSync } = require('fs');
 
 const {
   SIGN_TOOL_PATH = 'C:\\Program Files (x86)\\Windows Kits\\10\\bin\\x64\\signtool.exe',
@@ -9,44 +9,46 @@ const {
 
 const SITE = 'https://ffxivteamcraft.com';
 
-const importPfx = (certPath, password) => {
-  const command = [
-    ['certutil'],
-    ['-f'],
-    ['-p', `"${password}"`],
-    ['-importPfx', 'My', `"${certPath}"`, 'NoRoot']
-  ].map(sub => sub.join(' ')).join(' ');
-
+const signOnWindows = (filePath, name, certPath, password) => {
   try {
-    execSync(command, { stdio: 'inherit' });
+    execSync(
+      `certutil -f -p "${password}" -importPfx My "${certPath}" NoRoot`,
+      { stdio: 'inherit' }
+    );
   } catch {
     console.error('Unable to import certificate');
   }
-};
-
-const signBinary = (path, name) => {
-  const command = [
-    [`"${SIGN_TOOL_PATH}"`],
-    ['sign'],
-    ['/a'],
-    ['/s', 'My'],
-    ['/sm'],
-    ['/t', `"${TIMESTAMP_SERVER}"`],
-    ['/d', `"${name}"`],
-    ['/du', `"${SITE}"`],
-    [`"${path}"`]
-  ].map(sub => sub.join(' ')).join(' ');
 
   try {
-    execSync(command, { stdio: 'inherit' });
+    execSync(
+      `"${SIGN_TOOL_PATH}" sign /a /s My /sm /t "${TIMESTAMP_SERVER}" /d "${name}" /du "${SITE}" "${filePath}"`,
+      { stdio: 'inherit' }
+    );
   } catch {
-    console.error(`Signing ${path} failed`);
+    console.error(`Signing ${filePath} failed`);
   }
 };
 
-exports.default = ({ path, name, cscInfo: { file, password } = {} }) => {
+const signOnLinux = (filePath, name, certPath, password) => {
+  const tmp = `${filePath}.signed`;
+  try {
+    execSync(
+      `osslsigncode sign -pkcs12 "${certPath}" -pass "${password}" -n "${name}" -i "${SITE}" -t "${TIMESTAMP_SERVER}" -in "${filePath}" -out "${tmp}"`,
+      { stdio: 'inherit' }
+    );
+    renameSync(tmp, filePath);
+  } catch {
+    console.error(`Signing ${filePath} failed`);
+  }
+};
+
+exports.default = ({ path, name, cscInfo }) => {
+  const { file, password } = cscInfo || {};
   if (!file) return;
 
-  importPfx(file, password);
-  signBinary(path, name, file);
+  if (platform() === 'win32') {
+    signOnWindows(path, name, file, password);
+  } else {
+    signOnLinux(path, name, file, password);
+  }
 };
